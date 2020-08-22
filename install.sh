76420f3227e87e3e42a9332ee2c9ef34541d8f51
shteou <shteou@gmail.com>
2020-08-22 23:14:54 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Install argo to the default namespace
+kubectl create ns argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml
+
+# Install a rolebinding with admin privs
+kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=argo:default -n argo
+
+
